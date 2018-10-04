62b41b6542f731a94ea3c082a818b05f23dfbcf0
AlexandreHampel <42447798+AlexandreHampel@users.noreply.github.com>
2018-10-04 16:57:42 -0300

    toppen

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
